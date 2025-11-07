e54886533806016bf724bdad8e65fd84608d2dc8
Leonid <leonilfilippenkov@gmail.com>
2025-11-07 15:24:22 +0200

    Correcting tests and adding new ones.

--- test/test_bin_searh.py
+++ test/test_bin_searh.py
@@ -1,13 +1,33 @@
-from src.bin_search import binSearch
+from contextlib import nullcontext as does_not_raise
+
+import pytest
+
+from src.bin_search import bin_search
 
 
-def test_middle():
-    assert binSearch([1, 2, 3, 4, 5], 4) == 3
+class TestBin:
+    @pytest.mark.parametrize(
+        "array, digit, expected_index, expectation",
+        [
+            ([1, 2, 3, 4, 5], 3, 2, does_not_raise()),
+            ([1, 2, 3, 4], 2, 1, does_not_raise()),
+            ([1, 2, 3, 4], 3, 2, does_not_raise()),
+            ([1], 1, 0, does_not_raise()),
+            ([1, 2], 1, 0, does_not_raise()),
+            ([1, 2], 2, 1, does_not_raise()),
+        ],
+    )
+    def test_middle(self, array, digit, expected_index, expectation):
+        with expectation:
+            assert bin_search(array, digit) == expected_index
 
+    def test_start(self):
+        assert bin_search([1, 2, 3, 4], 2) == 1
 
-def test_start():
-    assert binSearch([1, 2, 3, 4], 2) == 1
+    def test_empty_list(self):
+        assert bin_search([], 1) == -1
 
-
-def test_not_in_list():
-    assert binSearch([1, 2, 3, 4], 5) == -1
+    def test_incorrect_index(self):
+        assert bin_search([5], 3) == -1
+        assert bin_search([1, 2], 3) == -1
+        assert bin_search([1, 2, 3, 4], 5) == -1
